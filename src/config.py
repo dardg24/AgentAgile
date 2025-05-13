b79fc45abb427ca370535608ad0d031000666468
Daniel <danielrdegouveia@gmail.com>
2025-05-13 22:55:30 +0200

    NewType for actions

--- src/config.py
+++ src/config.py
@@ -11,3 +11,13 @@ SLACK_CHANNEL_ID = os.getenv("SLACK_CHANNEL_ID")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 APP_TOKEN = os.getenv("APP_TOKEN")
 BOARD_ID = os.getenv('BOARD_ID')
+
+# NewTypeHints for functions
+from typing import NewType
+
+CardId = NewType('CardId', str)
+ListId = NewType('ListId', str)
+BoardID = NewType('BoardID', str)
+CardName = NewType('CardName', str)
+PositionCard = NewType('PositionCard', float)
+DescriptionCard = NewType('DescriptionCard', str)
